4ccef88b189e0f8dfaf2f85c870e41ee625a4958
haymax <haymax@aliyun.com>
2019-10-09 15:12:26 +0000

    中文的错误提示信息

--- config/app.php
+++ config/app.php
@@ -80,7 +80,7 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'ZH-CN',
 
     /*
     |--------------------------------------------------------------------------
@@ -158,9 +158,13 @@ return [
         Illuminate\Redis\RedisServiceProvider::class,
         Illuminate\Auth\Passwords\PasswordResetServiceProvider::class,
         Illuminate\Session\SessionServiceProvider::class,
-        Illuminate\Translation\TranslationServiceProvider::class,
+
+        //Illuminate\Translation\TranslationServiceProvider::class,
+        Overtrue\LaravelLang\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+
+
 
         /*
          * Package Service Providers...
