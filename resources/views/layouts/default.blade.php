96f1d984afe7afde7faf46e84610f906cb12b94f
haymax <haymax@aliyun.com>
2019-10-09 02:19:22 +0000

    切割头部和尾部子视图

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -10,11 +10,13 @@
 </html>
 --}}
 
-<!DOCTYPE html>
+{{--
+  <!DOCTYPE html>
 <html>
   <head>
     <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
   </head>
   <body>
 
@@ -33,4 +35,23 @@
     </div>
   </body>
 </html>
+ --}}
 
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+  </head>
+
+  <body>
+    @include('layouts._header')
+
+    <div class="container">
+      <div class="offset-md-1 col-md-10">
+        @yield('content')
+        @include('layouts._footer')
+      </div>
+    </div>
+  </body>
+</html>
