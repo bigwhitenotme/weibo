055d1bd55aa19344046696685b72bc490a7baf09
haymax <haymax@aliyun.com>
2019-10-10 12:19:32 +0000

    关注和粉丝列表页面

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,7 @@ Route::post('password/reset',
 
 
 Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
+
+
+Route::get('/users/{user}/followings', 'UsersController@followings')->name('users.followings');
+Route::get('/users/{user}/followers', 'UsersController@followers')->name('users.followers');
